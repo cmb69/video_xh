20565b5eb1866f25d297f9d569ae4775df67860d
cmb69 <cmbecker69@gmx.de>
2012-05-01 00:24:01 +0000

    added default.php

---
+++ languages/default.php
@@ -0,0 +1,17 @@
+<?php
+
+$plugin_tx['video']['message_no_js']="<strong>Enable Javascript for better/any viewing experience!</strong>";
+
+$plugin_tx['video']['syscheck_title']="System check";
+$plugin_tx['video']['syscheck_phpversion']="PHP version &ge; %s";
+$plugin_tx['video']['syscheck_extension']="Extension '%s' loaded";
+$plugin_tx['video']['syscheck_encoding']="Encoding 'UTF-8' configured";
+$plugin_tx['video']['syscheck_magic_quotes']="Magic quotes runtime off";
+$plugin_tx['video']['syscheck_writable']="Folder '%s' writable";
+
+$plugin_tx['video']['cf_use_cdn']="Whether to use the latest version of VIDEO.JS via CDN: \"1\" (yes) or \"0\" (no, use local version instead).";
+$plugin_tx['video']['cf_folder_video']="A folder relative to the CMSimple root folder, where the video files are stored. If left empty, the files are expected in \"Folders Media\" since CMSimple_XH 1.5 or in \"Folders Downloads\" for older versions.";
+$plugin_tx['video']['cf_default_width']="The default width of the videos, if not explicitely specified in the plugin call.";
+$plugin_tx['video']['cf_default_height']="The default height of the videos, if not explicitely specified in the plugin call.";
+
+?>
