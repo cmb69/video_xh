4a807651a0ee423c0f9e02ce169db5df9e926631
cmb69 <cmbecker69@gmx.de>
2012-05-01 13:16:48 +0000

    actually added the czech language file ;)

---
+++ languages/cs.php
@@ -0,0 +1,15 @@
+<?php
+
+$plugin_tx['video']['message_no_js']="<strong>Zapnout Javascript pro lepší zážitek sledování!</strong>";
+$plugin_tx['video']['syscheck_title']="System check";
+$plugin_tx['video']['syscheck_phpversion']="PHP verze &ge; %s";
+$plugin_tx['video']['syscheck_extension']="Rozšíření '%s' načteno";
+$plugin_tx['video']['syscheck_encoding']="Kodování 'UTF-8' nastaveno";
+$plugin_tx['video']['syscheck_magic_quotes']="Magic quotes runtime off";
+$plugin_tx['video']['syscheck_writable']="Adresáře '%s' jsou zapisovatelné";
+$plugin_tx['video']['cf_use_cdn']="Má se použít nejnovější verze VIDEO.JS přes CDN: \"1\" (ano) nebo \"0\" (ne, použít lokální verzi).";
+$plugin_tx['video']['cf_folder_video']="Složky ve vztahu k CMSimple kořenové složce, kde jsou uloženy video soubory. Je-li prázdné, jsou soubory očekávány ve \"Složce media\", od CMSimple_XH 1.5 nebo ve složce \"Downloads\" ve starších verzích.";
+$plugin_tx['video']['cf_default_width']="Výchozí šířka videa, pokud není upřesněno ve volání pluginu.";
+$plugin_tx['video']['cf_default_height']="Výchozí výška videa, pokud není upřesněno ve volání pluginu.";
+
+?>
