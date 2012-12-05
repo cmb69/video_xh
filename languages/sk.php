497ce115e5e912ce85e081fed1d58405d78c6ade
cmb69 <cmbecker69@gmx.de>
2012-12-05 19:08:36 +0000

    added slovak translation (contributed by Tata)

---
+++ languages/sk.php
@@ -0,0 +1,32 @@
+<?php
+
+$plugin_tx['video']['message_no_js']="<strong>Povoľte Javascript pre lepšie zobrazovanie!</strong>";
+$plugin_tx['video']['menu_main']="Výzva pre konštruktor";
+$plugin_tx['video']['syscheck_title']="Kontrola systému";
+$plugin_tx['video']['syscheck_phpversion']="PHP verzia &ge; %s";
+$plugin_tx['video']['syscheck_extension']="Rozšírenie '%s' načítané";
+$plugin_tx['video']['syscheck_encoding']="Kódovanie 'UTF-8' nastavené";
+$plugin_tx['video']['syscheck_magic_quotes']="Magic quotes runtime off";
+$plugin_tx['video']['syscheck_writable']="Adresár '%s' umožňuje zápis";
+$plugin_tx['video']['label_name']="Video";
+$plugin_tx['video']['label_preload']="Predčítanie";
+$plugin_tx['video']['label_autoplay']="Autoplay";
+$plugin_tx['video']['label_loop']="Slučka";
+$plugin_tx['video']['label_controls']="Ovládanie";
+$plugin_tx['video']['label_call']="Výzva pre plugin";
+$plugin_tx['video']['label_width']="Šírka";
+$plugin_tx['video']['label_height']="Výška";
+$plugin_tx['video']['preload_auto']="Auto";
+$plugin_tx['video']['preload_metadata']="Metadata";
+$plugin_tx['video']['preload_none']="Žiadne";
+$plugin_tx['video']['cf_use_cdn']="Má sa použiť najnovšia verzia VIDEO.JS via CDN: \"1\" (áno) alebo \"0\" (nie, použiť lokálnu verziu).";
+$plugin_tx['video']['cf_folder_video']="Cesta k adresáru s videom relatívna voči koreňovému adresáru CMSimple. Ak necháte prázdne, predpokladá sa, že súbory videa budú uložené v adresári \"Media\" od CMSimple_XH 1.5 alebo v adresári \"Downloads\" pri starších verziách CMSimple.";
+$plugin_tx['video']['cf_prefer_flash']="Uprednostniť flash video pred HTML5 videom?: \"1\" (áno) alebo \"0\" (nie). Toto štandardné nastavenie sa môže prepísať individuálne pri volaní každého videa.";
+$plugin_tx['video']['cf_default_controls']="Má video obsahovať aj avládacie plrvky?: \"1\" (áno) alebo \"0\" (nie). Toto štandardné nastavenie sa môže prepísať individuálne pri volaní každého videa.";
+$plugin_tx['video']['cf_default_preload']="Má sa video načítať pri načítaní stránky?: \"auto\" (video sa začne načítavať spolu so stránkou, ak to prehliadač povolí), \"metadata\" (načítajú sa iba metadata) alebo \"none\" (žiadne dáta viadea sa nenačítajú). Toto štandardné nastavenie sa môže prepísať individuálne pri volaní každého videa.";
+$plugin_tx['video']['cf_default_autoplay']="Má sa video spustiť po načítaní stránky?: \"1\" (áno) alebo \"0\" (nie). Toto štandardné nastavenie sa môže prepísať individuálne pri volaní každého videa. Autoplay nepodporujú Apple iOS zariadenia.";
+$plugin_tx['video']['cf_default_loop']="Má sa video po ukončení prehrávania spustiť znovu?: \"1\" (áno) alebo \"0\" (nie). Toto štandardné nastavenie sa môže prepísať individuálne pri volaní každého videa.";
+$plugin_tx['video']['cf_default_width']="Šírka videa. Toto štandardné nastavenie sa môže prepísať individuálne pri volaní každého videa.";
+$plugin_tx['video']['cf_default_height']="Výška videa. Toto štandardné nastavenie sa môže prepísať individuálne pri volaní každého videa.";
+
+?>
