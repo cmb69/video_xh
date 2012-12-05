0897ab7650832fc177794f60d00043457cd3c96d
cmb69 <cmbecker69@gmx.de>
2012-12-05 14:52:45 +0000

    added German translation

---
+++ languages/de.php
@@ -0,0 +1,38 @@
+<?php
+
+$plugin_tx['video']['message_no_js']="<div><strong>Aktivieren Sie Javascript um das Video besser bzw. überhaupt sehen zu können!</strong></div>";
+
+$plugin_tx['video']['menu_main']="Aufruf-Assistent";
+
+$plugin_tx['video']['syscheck_title']="Systemprüfung";
+$plugin_tx['video']['syscheck_phpversion']="PHP-Version &ge; %s";
+$plugin_tx['video']['syscheck_extension']="Erweiterung '%s' geladen";
+$plugin_tx['video']['syscheck_encoding']="UTF-8-Kodierung eingestellt";
+$plugin_tx['video']['syscheck_magic_quotes']="Magic quotes runtime off";
+$plugin_tx['video']['syscheck_writable']="Ordner '%s' beschreibbar";
+
+$plugin_tx['video']['label_name']="Video";
+$plugin_tx['video']['label_preload']="Vorladen";
+$plugin_tx['video']['label_autoplay']="Auto-Wiedergabe";
+$plugin_tx['video']['label_loop']="Wiederholen";
+$plugin_tx['video']['label_controls']="Steuerelemente";
+$plugin_tx['video']['label_call']="Plugin-Aufruf";
+$plugin_tx['video']['label_width']="Breite";
+$plugin_tx['video']['label_height']="Höhe";
+
+$plugin_tx['video']['preload_auto']="Auto";
+$plugin_tx['video']['preload_metadata']="Metadaten";
+$plugin_tx['video']['preload_none']="Nichts";
+
+$plugin_tx['video']['cf_use_cdn']="Ob die neuste Version von VIDEO.JS per CDN verwendet werden soll: \"1\" (ja) oder \"0\" (nein, lokale Version verwenden).";
+$plugin_tx['video']['cf_folder_video']="Ein Ordner relativ zum CMSimple-Installationsordner, wo die Videodateien gespeichert sind. Ist dieser Eintrag leer, dann werden die Dateien seit CMSimple_XH 1.5 in \"Folders Media\" oder bei älteren Versionen in \"Folders Downloads\" erwartet.";
+$plugin_tx['video']['cf_auto_resize']="Ob das Video automatisch auf die Größe des umgebenden Elements skaliert werden soll: \"1\" (ja) oder \"0\" (nein).";
+$plugin_tx['video']['cf_prefer_flash']="Ob Flash-Video dem HTML5-Video vorgezogen werden soll: \"1\" (ja) oder \"0\" (nein).";
+$plugin_tx['video']['cf_default_controls']="Ob der Player Elemente zur Steuerung durch den Benutzer anzeigen soll: \"1\" (ja) oder \"0\" (nein). Diese Voreinstellung kann individuell für jedes Video im Plugin-Aufruf überschrieben werden.";
+$plugin_tx['video']['cf_default_preload']="Ob und wie das Video geladen werden soll, wenn die Seite angezeigt wird: \"auto\" (unverzüglich mit dem Download beginnen, wenn der Browser dies erlaubt), \"metadata\" (nur die Metadaten des Videos laden) oder \"none\" (keine Videodaten im Voraus herunter laden). Diese Voreinstellung kann individuell für jedes Video im Plugin-Aufruf überschrieben werden.";
+$plugin_tx['video']['cf_default_autoplay']="Ob das Video automatisch abgespielt werden soll, sobald die Seite geladen wurde: \"1\" (ja) oder \"0\" (nein). Diese Voreinstellung kann individuell für jedes Video im Plugin-Aufruf überschrieben werden. Das automatische Abspielen wird von Apple iOS Geräten nicht unterstützt.";
+$plugin_tx['video']['cf_default_loop']="Ob das Video in einer Schleife abgespielt werden soll: \"1\" (ja) oder \"0\" (nein). Diese Voreinstellung kann individuell für jedes Video im Plugin-Aufruf überschrieben werden..";
+$plugin_tx['video']['cf_default_width']="Die Breite des Videos. Diese Voreinstellung kann individuell für jedes Video im Plugin-Aufruf überschrieben werden.";
+$plugin_tx['video']['cf_default_height']="Die Höhe des Videos. Diese Voreinstellung kann individuell für jedes Video im Plugin-Aufruf überschrieben werden.";
+
+?>
