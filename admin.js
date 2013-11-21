447d157b626be7bbad301c08e50658ee0ca320c5
cmb69 <cmbecker69@gmx.de>
2013-11-21 12:32:55 +0000

    refactored JS to be namespaced

--- admin.js
+++ admin.js
@@ -1,57 +1,87 @@
-// $Id$
-
 /**
- * Back-end JS of Video_XH.
+ * Back-end JavaScript of Video_XH.
  *
- * Copyright (c) 2012-2013 Christoph M. Becker (see license.txt)
+ * @author   Christoph M. Becker <cmbecker69@gmx.de>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @version  SVN: $Id$
  */
 
+    /**
+     * @namespace video
+     */
 
-var Video = {
-
-    fields: {},
-
-
-    init: function() {
-        var keys, i, key;
-
-        keys = ['name', 'preload', 'autoplay', 'loop', 'controls',
-            'width', 'height', "resize", 'call'];
-        for (i = 0; i < keys.length; i++) {
-            key = keys[i];
-            this.fields[key] = document.getElementById('video_' + key);
-            if (key != 'call') {
-                this.fields[key].onchange = this.buildPluginCall;
-            }
-        }
-        this.buildPluginCall();
-    },
-
-
-    buildPluginCall: function() {
-        var flds = Video.fields, opts = [], keys, key, i;
-
-        keys = ['width', 'height'];
-        for (i = 0; i < keys.length; i++) {
-            key = keys[i];
-            if (flds[key].value != '') {
-                opts.push(key + '=' + flds[key].value);
-            }
-        }
-
-        opts.push('preload=' + flds['preload'].value);
-        opts.push('resize=' + flds["resize"].value);
-
-        keys = ['autoplay', 'loop', 'controls'];
-        for (i = 0; i < keys.length; i++) {
-            key = keys[i];
-            opts.push(key + '=' + (flds[key].checked ? '1' : '0'));
-        }
-
-        flds.call.value = '{{{PLUGIN:video(\'' + flds.name.value + '\', \''
-            + opts.join('&') + '\');}}}';
-    }
-
+if (typeof video == "undefined") {
+    var video = {}
 }
 
-Video.init();
+
+/**
+ * The relevant elements of the call builder.
+ *
+ * @type {Array.<HTMLInputElement>}
+ */
+video.callBuilderElements = {};
+
+
+/**
+ * Initializes the call builder.
+ */
+video.initCallBuilder = function() {
+    var keys, i, len, key, element;
+
+    keys = [
+        "name", "preload", "autoplay", "loop", "controls",
+        "width", "height", "resize", "call"
+    ];
+    for (i = 0, len = keys.length; i < len; i++) {
+        key = keys[i];
+        element = document.getElementById("video_" + key);
+        video.callBuilderElements[key] = element;
+        if (key != 'call') {
+            element.onchange = video.buildPluginCall;
+        } else {
+            element.onclick = function() {this.select()}
+        }
+    }
+    video.buildPluginCall();
+}
+
+
+/**
+ * Builds the plugin call.
+ */
+video.buildPluginCall = function() {
+    var elements = video.callBuilderElements,
+        opts = [],
+        keys, key, i, len;
+
+    keys = ["width", "height"];
+    for (i = 0, len = keys.length; i < len; i++) {
+        key = keys[i];
+        if (elements[key].value != '') {
+            opts.push(key + '=' + elements[key].value);
+        }
+    }
+
+    opts.push("preload=" + elements["preload"].value);
+    opts.push("resize=" + elements["resize"].value);
+
+    keys = ["autoplay", "loop", "controls"];
+    for (i = 0, len = keys.length; i < len; i++) {
+        key = keys[i];
+        opts.push(key + "=" + (elements[key].checked ? "1" : "0"));
+    }
+
+    elements.call.value = "{{{PLUGIN:video('" + elements.name.value + "', '"
+        + opts.join('&') + "');}}}";
+}
+
+
+/*
+ * Registers the initialization of the call builder.
+ */
+if (typeof window.addEventListener != "undefined") {
+    window.addEventListener("load", video.initCallBuilder, false)
+} else if (typeof window.attachEvent != "undefined") {
+    window.attachEvent("onload", video.initCallBuilder);
+}
--- autosize.js
+++ autosize.js
@@ -1,35 +1,45 @@
 /**
  * Front-End JS of Video_XH.
  *
- * @version: $Id$
+ * @author   Christoph M. Becker <cmbecker69@gmx.de>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @version: SVN: $Id$
  */
 
+    /**
+     * @namespace video
+     */
 
-var video = {
+if (typeof video == "undefined") {
+    video = {}
+}
 
-    autosize: function(player, mode) {
-        var ar, mw;
 
-        if (mode != "shrink" && mode != "full") {
-            return;
+/**
+ * @param {Object}
+ * @param {String}
+ */
+video.autosize = function(player, mode) {
+    var ar, mw;
+
+    if (mode != "shrink" && mode != "full") {
+        return;
+    }
+    ar = player.width() / player.height();
+    mw = player.width();
+    function resize() {
+        var w;
+
+        w = document.getElementById(player.id).parentElement.offsetWidth;
+        if (mode == "shrink" && w > mw) {
+            w = mw;
         }
-        ar = player.width() / player.height();
-        mw = player.width();
-        function resize() {
-            var w;
-
-            w = document.getElementById(player.id).parentElement.offsetWidth;
-            if (mode == "shrink" && w > mw) {
-                w = mw;
-            }
-            player.width(w).height(Math.round(w / ar));
-        }
-        resize();
-        if (window.addEventListener) {
-            window.addEventListener("resize", resize, false);
-        } else if (window.attachEvent)  {
-            window.attachEvent("onresize", resize);
-        }
-
+        player.width(w).height(Math.round(w / ar));
+    }
+    resize();
+    if (typeof window.addEventListener != "undefined") {
+        window.addEventListener("resize", resize, false);
+    } else if (typeof window.attachEvent != "undefined")  {
+        window.attachEvent("onresize", resize);
     }
 }
