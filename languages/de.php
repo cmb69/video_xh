5d3d0800af7cae6d040f8fd4a916678f4233533a
cmb69 <cmbecker69@gmx.de>
2013-12-12 13:13:28 +0000

    cleaned up config.php, en.php and de.php

--- languages/de.php
+++ languages/de.php
@@ -1,54 +1,46 @@
 <?php
 
-$plugin_tx['video']['message_no_js']="Aktivieren Sie Javascript um das Video besser bzw. überhaupt sehen zu können!";
-
-$plugin_tx['video']['menu_main']="Aufruf-Assistent";
-
-$plugin_tx['video']['syscheck_title']="Systemprüfung";
-$plugin_tx['video']['syscheck_phpversion']="PHP-Version &ge; %s";
-$plugin_tx['video']['syscheck_extension']="Erweiterung '%s' geladen";
-$plugin_tx['video']['syscheck_encoding']="UTF-8-Kodierung eingestellt";
-$plugin_tx['video']['syscheck_magic_quotes']="Magic quotes runtime off";
-$plugin_tx['video']['syscheck_writable']="Ordner '%s' beschreibbar";
-
+$plugin_tx['video']['align_center']="mittig";
+$plugin_tx['video']['align_left']="links";
+$plugin_tx['video']['align_right']="rechts";
+$plugin_tx['video']['error_missing']="Video \"%s\" fehlt!";
+$plugin_tx['video']['label_align']="Ausrichtung";
+$plugin_tx['video']['label_autoplay']="Auto-Wiedergabe";
+$plugin_tx['video']['label_call']="Plugin-Aufruf";
+$plugin_tx['video']['label_controls']="Steuerelemente";
+$plugin_tx['video']['label_height']="Höhe";
+$plugin_tx['video']['label_loop']="Wiederholen";
 $plugin_tx['video']['label_name']="Video";
 $plugin_tx['video']['label_preload']="Vorladen";
-$plugin_tx['video']['label_autoplay']="Auto-Wiedergabe";
-$plugin_tx['video']['label_loop']="Wiederholen";
-$plugin_tx['video']['label_controls']="Steuerelemente";
-$plugin_tx['video']['label_call']="Plugin-Aufruf";
-$plugin_tx['video']['label_width']="Breite";
-$plugin_tx['video']['label_height']="Höhe";
-$plugin_tx['video']['label_align']="Ausrichtung";
 $plugin_tx['video']['label_resize']="Skalierung";
-
+$plugin_tx['video']['label_width']="Breite";
+$plugin_tx['video']['menu_main']="Aufruf-Assistent";
+$plugin_tx['video']['message_no_js']="Aktivieren Sie Javascript um das Video besser bzw. überhaupt sehen zu können!";
 $plugin_tx['video']['preload_auto']="Auto";
 $plugin_tx['video']['preload_metadata']="Metadaten";
 $plugin_tx['video']['preload_none']="Nichts";
-
+$plugin_tx['video']['resize_full']="Voll";
 $plugin_tx['video']['resize_no']="Nein";
 $plugin_tx['video']['resize_shrink']="Verkleinern";
-$plugin_tx['video']['resize_full']="Voll";
-
-$plugin_tx['video']['align_left']="links";
-$plugin_tx['video']['align_center']="mittig";
-$plugin_tx['video']['align_right']="rechts";
-
 $plugin_tx['video']['subtitle_label']="Deutsch";
-
-$plugin_tx['video']['error_missing']="Video \"%s\" fehlt!";
-
+$plugin_tx['video']['syscheck_encoding']="UTF-8-Kodierung eingestellt";
+$plugin_tx['video']['syscheck_extension']="Erweiterung '%s' geladen";
+$plugin_tx['video']['syscheck_magic_quotes']="Magic quotes runtime off";
+$plugin_tx['video']['syscheck_phpversion']="PHP-Version &ge; %s";
+$plugin_tx['video']['syscheck_title']="Systemprüfung";
+$plugin_tx['video']['syscheck_writable']="Ordner '%s' beschreibbar";
+$plugin_tx['video']['cf_auto_hjs']="Ob die benötigten Skripte und Stylesheets immer geladen werden sollen (nötig, wenn Sie Videos im Template oder einer Newsbox verwenden wollen): \"1\" (ja) oder \"0\" (nein).";
+$plugin_tx['video']['cf_default_align']="Die Ausrichtung des Videos: \"left\" (links), \"center\" (mittig) or \"right\" (rechts). Diese Voreinstellung kann individuell für jedes Video im Plugin-Aufruf überschrieben werden.";
+$plugin_tx['video']['cf_default_autoplay']="Ob das Video automatisch abgespielt werden soll, sobald die Seite geladen wurde: \"1\" (ja) oder \"0\" (nein). Diese Voreinstellung kann individuell für jedes Video im Plugin-Aufruf überschrieben werden. Das automatische Abspielen wird von Apple iOS Geräten nicht unterstützt.";
+$plugin_tx['video']['cf_default_controls']="Ob der Player Elemente zur Steuerung durch den Benutzer anzeigen soll: \"1\" (ja) oder \"0\" (nein). Diese Voreinstellung kann individuell für jedes Video im Plugin-Aufruf überschrieben werden.";
+$plugin_tx['video']['cf_default_height']="Die Höhe des Videos. Diese Voreinstellung kann individuell für jedes Video im Plugin-Aufruf überschrieben werden.";
+$plugin_tx['video']['cf_default_loop']="Ob das Video in einer Schleife abgespielt werden soll: \"1\" (ja) oder \"0\" (nein). Diese Voreinstellung kann individuell für jedes Video im Plugin-Aufruf überschrieben werden..";
+$plugin_tx['video']['cf_default_preload']="Ob und wie das Video geladen werden soll, wenn die Seite angezeigt wird: \"auto\" (unverzüglich mit dem Download beginnen, wenn der Browser dies erlaubt), \"metadata\" (nur die Metadaten des Videos laden) oder \"none\" (keine Videodaten im Voraus herunter laden). Diese Voreinstellung kann individuell für jedes Video im Plugin-Aufruf überschrieben werden.";
+$plugin_tx['video']['cf_default_resize']="Skalierungsmodus: \"no\" (feste Größe gemäß Breite und Höhe), \"shrink\" (variable Größe als ob das Element max-width:100% hätte) oder \"full\" (variable Größe als ob das Element width:100% hätte).";
+$plugin_tx['video']['cf_default_width']="Die Breite des Videos. Diese Voreinstellung kann individuell für jedes Video im Plugin-Aufruf überschrieben werden.";
+$plugin_tx['video']['cf_folder_video']="Ein Ordner relativ zum CMSimple-Installationsordner, wo die Videodateien gespeichert sind. Ist dieser Eintrag leer, dann werden die Dateien seit CMSimple_XH 1.5 in \"Folders Media\" oder bei älteren Versionen in \"Folders Downloads\" erwartet.";
+$plugin_tx['video']['cf_prefer_flash']="Ob Flash-Video dem HTML5-Video vorgezogen werden soll: \"1\" (ja) oder \"0\" (nein).";
 $plugin_tx['video']['cf_use_cdn']="Ob die neuste Version von VIDEO.JS per CDN verwendet werden soll: \"1\" (ja) oder \"0\" (nein, lokale Version verwenden).";
 $plugin_tx['video']['cf_skin']="Der Name eines Skins (z.B. \"tube\" oder \"tube2\"). Leer lassen, um den Standard-Skin zu verwenden.";
-$plugin_tx['video']['cf_folder_video']="Ein Ordner relativ zum CMSimple-Installationsordner, wo die Videodateien gespeichert sind. Ist dieser Eintrag leer, dann werden die Dateien seit CMSimple_XH 1.5 in \"Folders Media\" oder bei älteren Versionen in \"Folders Downloads\" erwartet.";
-$plugin_tx['video']['cf_auto_hjs']="Ob die benötigten Skripte und Stylesheets immer geladen werden sollen (nötig, wenn Sie Videos im Template oder einer Newsbox verwenden wollen): \"1\" (ja) oder \"0\" (nein).";
-$plugin_tx['video']['cf_default_resize']="Skalierungsmodus: \"no\" (feste Größe gemäß Breite und Höhe), \"shrink\" (variable Größe als ob das Element max-width:100% hätte) oder \"full\" (variable Größe als ob das Element width:100% hätte).";
-$plugin_tx['video']['cf_prefer_flash']="Ob Flash-Video dem HTML5-Video vorgezogen werden soll: \"1\" (ja) oder \"0\" (nein).";
-$plugin_tx['video']['cf_default_controls']="Ob der Player Elemente zur Steuerung durch den Benutzer anzeigen soll: \"1\" (ja) oder \"0\" (nein). Diese Voreinstellung kann individuell für jedes Video im Plugin-Aufruf überschrieben werden.";
-$plugin_tx['video']['cf_default_preload']="Ob und wie das Video geladen werden soll, wenn die Seite angezeigt wird: \"auto\" (unverzüglich mit dem Download beginnen, wenn der Browser dies erlaubt), \"metadata\" (nur die Metadaten des Videos laden) oder \"none\" (keine Videodaten im Voraus herunter laden). Diese Voreinstellung kann individuell für jedes Video im Plugin-Aufruf überschrieben werden.";
-$plugin_tx['video']['cf_default_autoplay']="Ob das Video automatisch abgespielt werden soll, sobald die Seite geladen wurde: \"1\" (ja) oder \"0\" (nein). Diese Voreinstellung kann individuell für jedes Video im Plugin-Aufruf überschrieben werden. Das automatische Abspielen wird von Apple iOS Geräten nicht unterstützt.";
-$plugin_tx['video']['cf_default_loop']="Ob das Video in einer Schleife abgespielt werden soll: \"1\" (ja) oder \"0\" (nein). Diese Voreinstellung kann individuell für jedes Video im Plugin-Aufruf überschrieben werden..";
-$plugin_tx['video']['cf_default_width']="Die Breite des Videos. Diese Voreinstellung kann individuell für jedes Video im Plugin-Aufruf überschrieben werden.";
-$plugin_tx['video']['cf_default_height']="Die Höhe des Videos. Diese Voreinstellung kann individuell für jedes Video im Plugin-Aufruf überschrieben werden.";
 
 ?>
